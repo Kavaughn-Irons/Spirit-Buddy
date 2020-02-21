4ba26f0410bff93cfd50723a114ab789226b0458
kavaughn_irons <kavaughnawsomeness@gmail.com>
2020-02-20 19:51:48 -0500

    Added Settings Page and small changes.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Login from "./components/pages/login";
 import Home from "./components/pages/home";
 import Profile from "./components/pages/profile";
+import Settings from "./components/pages/settings";
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Route exact path="/" component={Login} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/profile" component={Profile} />
+        <Route exact path="/settings" component={Settings} />
       </div>
     </Router>
   );
