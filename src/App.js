80cfe5491be568328b28754e1c83507f98ddac74
kavaughn_irons <kavaughnawsomeness@gmail.com>
2020-02-26 15:43:56 -0500

    added messenger page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Login from "./components/pages/login";
 import Home from "./components/pages/home";
 import Profile from "./components/pages/profile";
 import Settings from "./components/pages/settings";
+import Messenger from "./components/pages/messenger";
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -17,6 +19,7 @@ function App() {
         <Route exact path="/home" component={Home} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/settings" component={Settings} />
+        <Route exact path="/messenger" component={Messenger} />
       </div>
     </Router>
   );
---
+++ src/components/pages/messenger.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import FullHeight from "react-full-height";
+import 'whatwg-fetch';
+import ProfilePlaceholder from "../../Assets/Profile-Placeholder.png";
+import ProgressBar from '../../../node_modules/progressbar.js'
+
+
+class Profile extends Component {
+
+
+
+constructor(){
+
+
+
+super()
+  this.state = {
+
+  profileHeight : '',
+  message: '',
+  height: ''
+
+
+  }
+
+}
+
+componentWillMount(){
+
+console.log(window.screen.height)
+
+
+
+}
+
+componentDidMount(){
+
+
+
+}
+
+
+Login(event){
+
+event.preventDefault()
+
+console.log("event")
+
+let getID = 1
+
+fetch('http://localhost:5000/api/test', {
+  mode : 'cors',
+  method: 'post',
+  headers: {
+    'access-Control-Allow-Origin':  'https://localhost:5000/api/test',
+    'Content-Type': 'application/json'
+  },body: JSON.stringify({getID}) }).then(res => res.json()).then(json => console.log(json.success))
+
+
+}
+
+render() {
+
+
+    const containerStyle = {
+      width : '100%',
+      margin: '0px',
+      padding: '0px',
+      height: '2200px',
+      overflowX: 'hidden',
+      backgroundImage : 'linear-gradient(180deg,#6978E9,#6D04A6)',
+    }
+
+    return (
+     
+        <div style={containerStyle} className="no-gutter">
+        
+        </div>
+    );
+  }
+
+}
+
+export default Profile;
